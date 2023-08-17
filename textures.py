03b843f95c7f299862ec9b5f6185141988107261
Shamsul Arifin <arifinbd6@gmail.com>
2023-08-17 20:33:41 +0600

    Added shading

--- textures.py
+++ textures.py
@@ -9,6 +9,8 @@ class Textures:
 
         # load texture
         self.texture_0 = self.load('frame.png')
+        # self.texture_0 = self.load('test.png')
+
 
         # assign texture unit
         self.texture_0.use(location=0)
